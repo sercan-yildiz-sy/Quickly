1363352d0e09fdc23878437606c980ea0cd70ccc
sercanyildiz96 <sercan_yildiz@outlook.com>
2025-01-28 11:29:19 -0500

    style

--- Pages/ProjectListPage.xaml.cs
+++ Pages/ProjectListPage.xaml.cs
@@ -11,5 +11,15 @@ namespace Quicky.Pages
             InitializeComponent();
             BindingContext = model;
         }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = entry.Text;
+        }
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
     }
 }
